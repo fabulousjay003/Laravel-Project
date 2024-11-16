9f820ba21a61e94d7f93eb16b18c0e31a0d458d6
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2024-11-15 18:16:27 -0600

    worked on routing,layout and nav component

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <x-nav-link href='/'> 
+    Home
+    </x-nav-link>   
+    <x-nav-link href='/about'>
+     About
+    </x-nav-link>    
+    <x-nav-link href='/contact'>
+     Contact
+    </x-nav-link>
+{{$slot}}
+</body>
+
+</html>

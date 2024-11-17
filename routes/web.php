f2922da91400ad9a0d8f16a4ea88a2cfb8a58239
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2024-11-17 16:55:37 -0600

    started working on job listing

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view(
+        'home'
+    );
 });
 
 
-Route::get('/about', function () {
-    return  view('about');
+Route::get('/jobs', function () {
+    return  view(
+        'jobs',
+        [
+            'jobs' => [
+                [
+                    'title' => 'Director',
+                    'salary' => '$50,000'
+
+                ],
+                [
+                    'title' => 'Programmer',
+                    'salary' => '$100,000'
+
+                ],
+                [
+                    'title' => 'Teacher',
+                    'salary' => '$40,000'
+
+                ]
+            ]
+        ]
+    );
 });
 
 

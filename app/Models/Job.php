b355fa9c790eaae37e5214f961932540bd89744e
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2024-11-20 21:30:54 -0600

    started eorking on eloquent, factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,40 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job
+
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return   [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$100,000'
-
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-
-            ]
-        ];
-    }
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    }
+    use Hasfactory;
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -5,5 +5,4 @@
 
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
     <p>This job pays {{ $job['salary'] }} per year</p>
-
 </x-layout>

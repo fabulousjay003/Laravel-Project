76123b47457ed59df8316a69540185dc120c82a9
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2024-12-12 02:51:31 +0100

    worked on Aunthentication

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,23 @@
 <?php
 
-use App\Http\Controllers\JobController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 
 
 Route::view('/', 'home');
 
-
-// Route::controller(JobController::class)->group(
-//     function () {
-//         // //all jobs or index
-//         // Route::get('/jobs', 'index');
-
-//         // //create job
-//         // Route::get('/jobs/create', 'create');
-
-
-//         // //edit job
-//         // Route::get('/jobs/{job}/edit', 'edit');
-
-
-//         // //show job
-//         // Route::get('/jobs/{job}', 'show');
-
-//         // //store job
-//         // Route::post('/jobs', 'store');
-
-//         // //update
-//         // Route::patch('/jobs/{job}', 'update');
-
-
-//         // //Destroy
-//         // Route::delete('/jobs/{job}',  'destroy');
-
-//     }
-// );
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
-Route::view('/contact', 'contact');
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+
+Route::post('/logout', [SessionController::class, 'destroy']);

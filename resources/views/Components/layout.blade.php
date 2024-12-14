f45f7820ed9a3a8d67c58034c9fcc3201a128001
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2024-12-14 23:45:03 +0100

    started working on the assets bundling

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src='https://cdn.tailwindcss.com'></script>
+
 </head>
 
 <body class="h-full">
